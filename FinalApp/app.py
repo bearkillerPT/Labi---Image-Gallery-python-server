08baaee3c7e3462dd36bb07c6c5619b2b7308b0f
André Patacas <andrepatacas@ua.pt>
2019-05-09 16:54:47 -0400

    Criação da pasta FinalApp

---
+++ FinalApp/app.py
@@ -0,0 +1,29 @@
+import cherrypy
+from cherrypy import tools
+import os
+from DbCommunicator import DbCommunicator 
+comm = DbCommunicator('app_db.db')
+class HelloWorld(object):
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def list(self, type = None, name = None, color = None):
+        request_body = {} # this form of create dict request will leave all unused parameter empty
+        if(type):
+            request_body['type'] = type
+        if(name):
+            request_body['name'] = name
+        if(color):
+            request_body['color'] = color.json()
+        return comm.request(request_body)
+    @cherrypy.expose
+    def index(self):
+        return "Hello World!"
+
+PATH = os.path.abspath(os.path.dirname(__file__))
+config = {
+    '/images':{
+        'tools.staticdir.on' : True,
+        'tools.staticdir.dir' : os.path.join(PATH, "images")
+    }
+}
+cherrypy.quickstart(HelloWorld(),'/',config=config)
