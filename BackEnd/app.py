9f2efa60ddf6d3ce6177708dd1f8f04b6a8bd042
Andre Patacas <andrepatacas@ua.pt>
2019-06-05 11:38:50 +0100

    correção de erros no app.py

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -19,7 +19,7 @@ class app(object):
         if(color):
             request_body['color'] = json.loads(color)
         if(thr):
-            request_body['thr'] = thr*100
+            request_body['thr'] = thr
         if(page):
             request_body['page'] = page
         if(per_page):
