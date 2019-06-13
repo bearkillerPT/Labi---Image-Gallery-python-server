b03b6aae0ef1f3c7bc7eaff7d3de6f18f644cccd
Andre Patacas <andrepatacas@ua.pt>
2019-06-13 11:08:01 +0100

    Finalização dos testes funcionais

--- Tests/test_func_tests.py
+++ Tests/test_func_tests.py
@@ -3,12 +3,15 @@ import random
 import os
 import requests
 import json
-
+from hashlib import md5
+import sqlite3
 NUMTESTS = 10
 image_list = []
 for filename in os.listdir("./TestImages"):
   image_list.append(open("./TestImages/" + filename, 'rb').read())
 
+#Como a funcionalidade dos metodos relativos ao requests já é testada nos testes unitarios
+# aqui apenas vão ser testadas as mensagens de erro
 def test_put():
   url = 'http://127.0.0.1:10002/put'
   for _ in range(NUMTESTS):
@@ -28,4 +31,34 @@ def test_put():
   res = json.loads(r.text)
   assert res[0] == "Any class detected"
 
-# todos os outros testes funcionais são analogos aos testes das funções respetivas
+
+def test_get():
+  r = requests.get('http://127.0.0.1:10002/get?id=' + md5(bytes(random.randint(0,10))).hexdigest())
+  assert r.json() == "Image Not Found"
+
+def test_list():
+  request_list = [
+    "http://127.0.0.1:10002/list?type=name",
+    "http://127.0.0.1:10002/list?type=detect",
+    "http://127.0.0.1:10002/list?type=detect&thr=1",
+    "http://127.0.0.1:10002/list?type=detect&name=asd",
+    "http://127.0.0.1:10002/list?type=detect&name=asd&thr=2",
+    "http://127.0.0.1:10002/list?type=detect&name=as",
+    "http://127.0.0.1:10002/list?type=detect&name=as123123",
+    "http://127.0.0.1:10002/list?type=detect&name=asasdss&page=1",
+    "http://127.0.0.1:10002/list?type=detect&name=asasdss&page=1&per_page=32",
+  ]
+  for _ in range(NUMTESTS):
+    request = random.choice(request_list)
+    r = requests.get(request)
+    assert r.json() == "Bad Call"
+
+
+def test_garbage_collect():
+  for filename in os.listdir("./../BackEnd/images"):
+    os.remove('./../BackEnd/images/'+filename)
+    db = sqlite3.connect("./../BackEnd/app_db.db")
+    db.execute('delete from RelImgCaract')
+    db.execute('delete from Imagens')
+    db.commit()
+    db.close()
